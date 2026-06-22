29a66354ec1ae688e4f8643346842d319e51b96a
srisahithijakka-maker <srisahithijakka@gmail.com>
2026-06-23 02:53:11 +0530

    Add Logger class

---
+++ Week1/DesignPatterns/SingletonPattern/Logger.java
@@ -0,0 +1,12 @@
+public class Logger {
+
+    private static Logger instance = new Logger();
+
+    private Logger() {
+
+    }
+
+    public static Logger getInstance() {
+        return instance;
+    }
+}

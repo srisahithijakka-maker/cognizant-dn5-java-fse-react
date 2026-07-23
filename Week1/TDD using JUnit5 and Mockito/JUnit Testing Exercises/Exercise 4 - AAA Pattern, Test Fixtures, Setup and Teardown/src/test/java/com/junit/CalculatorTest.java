01a54100358576f84d79ce2ae3f2f5e0380a0113
srisahithijakka-maker <srisahithijakka@gmail.com>
2026-07-23 19:26:24 +0530

    Add files via upload

---
+++ Week1/TDD using JUnit5 and Mockito/JUnit Testing Exercises/Exercise 4 - AAA Pattern, Test Fixtures, Setup and Teardown/src/test/java/com/junit/CalculatorTest.java
@@ -0,0 +1,38 @@
+package com.junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Setting up test...");
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testMultiply() {
+
+        // Arrange
+        int a = 5;
+        int b = 4;
+
+        // Act
+        int result = calculator.multiply(a, b);
+
+        // Assert
+        assertEquals(20, result);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Cleaning up after test...");
+    }
+}

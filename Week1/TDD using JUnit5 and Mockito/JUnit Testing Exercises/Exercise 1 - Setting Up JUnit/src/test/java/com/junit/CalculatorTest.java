1cdfef490752c9b8ba4e7c5fdb4928bd9e1a7c86
srisahithijakka-maker <srisahithijakka@gmail.com>
2026-07-23 18:51:37 +0530

    Add files via upload

---
+++ Week1/TDD using JUnit5 and Mockito/JUnit Testing Exercises/Exercise 1 - Setting Up JUnit/src/test/java/com/junit/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void testSetup() {
+        System.out.println("JUnit setup successful.");
+        System.out.println("Test executed successfully.");
+    }
+}
